8a0e7e8ea045145cf27bea1b72f20110c4ec42e4
nkdblack94 <nkdblack1294@gmail.com>
2020-06-22 14:01:58 +0700

    thực hành Stack

---
+++ src/test/java/GenericStackClient.java
@@ -0,0 +1,8 @@
+public class GenericStackClient extends MyGenericStack {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        stackOfIStrings();
+    }
+}
